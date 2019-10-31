e8ece0a30d30f477908911ed72acd4b168308dc1
Natalia Soto <nataliamelissas@gmail.com>
2019-10-31 16:43:11 -0600

    Added index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+d3.csv("data/EQ_data.csv").then( data => {
+    console.log(data)
+
+    // Create hierarchy dataset for Sunburst Zoom (Natalia)
+
+    // Setup data for Scatterplot (Viraj)
+
+    // Setup data for Map (Reza)
+
+});
