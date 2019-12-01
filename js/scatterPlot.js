74727f1ad9a54f7ea74a1d1f7f14e803f8caf1b2
Reza <m.sheibani@utah.edu>
2019-12-01 13:12:14 -0700

    Visual improvements for scatterplot

--- js/scatterPlot.js
+++ js/scatterPlot.js
@@ -3,8 +3,8 @@ class GapPlot {
 
     constructor(data,updateHighlight,filterMap) {
         this.margin = { top: 20, right: 20, bottom: 50, left: 80 };
-        this.width = 700 - this.margin.left - this.margin.right;
-        this.height = 500 - this.margin.top - this.margin.bottom;
+        this.width = 720 - this.margin.left - this.margin.right;
+        this.height = 520 - this.margin.top - this.margin.bottom;
         this.data = data;
 
         for (let key in data) {
@@ -32,17 +32,19 @@ class GapPlot {
     }
     drawPlot(){
         d3.select('#dataviz-scatterplot').append('div').attr('id', 'chart-view');
-        d3.select('#chart-view')
+    let mainSvg=d3.select('#chart-view')
             .append('svg').classed('plot-svg', true)
-            // .style("margin", "2rem")
             .attr("width", this.width + this.margin.left + this.margin.right)
             .attr("height", this.height + this.margin.top + this.margin.bottom);
 
-        // d3.select('#chart-view')
-        //     .append('div')
-        //     .attr("class", "tooltip")
-        //     .style("opacity", 0);
-
+            mainSvg.attr('xlmns','http://www.w3.org/2000/svg').attr('xmlns:xlink','http://www.w3.org/1999/xlink').append('filter').attr('id','shadow').append('feGaussianBlur').attr('in','SourceGraphic').attr('stdDeviation',3)
+        mainSvg.append('g').attr('id','BG_g').append('rect').attr('x','1%').attr('y','1%')
+            .attr('width','98%').attr('height','98%')
+            .style('filter','url(#shadow)').style('fill','#a2a2a2').classed('BG_rect',true);
+        mainSvg.select('#BG_g').append('rect').attr('x','2%').attr('y','2%')
+            .attr('width','96%').attr('height','96%')
+            .style('fill','white').classed('BG_rect',true);
+        d3.select('#chart-view').select('.plot-svg').append('g').classed("brush", true);
         let svgGroup = d3.select('#chart-view').select('.plot-svg').append('g').classed('wrapper-group', true);
 
         svgGroup.append('g').attr('id', 'axesX');
@@ -56,7 +58,7 @@ class GapPlot {
 
         cWrap.append('div').classed('c-label', true)
             .append('text')
-            .text('Circle Size');
+            .text('Circle Size').classed('SP_DD_text',true);
 
         cWrap.append('div').attr('id', 'dropdown_c').classed('dropdown', true).append('div').classed('dropdown-content', true)
             .append('select');
@@ -65,7 +67,7 @@ class GapPlot {
 
         xWrap.append('div').classed('x-label', true)
             .append('text')
-            .text('X Axis Data');
+            .text('X Axis Data').classed('SP_DD_text',true);
 
         xWrap.append('div').attr('id', 'dropdown_x').classed('dropdown', true).append('div').classed('dropdown-content', true)
             .append('select');
@@ -74,7 +76,7 @@ class GapPlot {
 
         yWrap.append('div').classed('y-label', true)
             .append('text')
-            .text('Y Axis Data');
+            .text('Y Axis Data').classed('SP_DD_text',true);
 
         yWrap.append('div').attr('id', 'dropdown_y').classed('dropdown', true).append('div').classed('dropdown-content', true)
             .append('select');
@@ -111,8 +113,8 @@ class GapPlot {
         this.drawLegend(minSize, maxSize);
 
         let xScale = d3.scaleLinear()
-            .domain([0, maxvals[1]])
-            .range([this.margin.left, this.width+this.margin.left]);
+            .domain([minvals[1], maxvals[1]])
+            .range([this.margin.left, this.width+this.margin.left-this.margin.right-10]);
 
 
         let xAxis = d3.axisBottom();
@@ -123,13 +125,13 @@ class GapPlot {
         svg9 = svg9.enter().merge(svg9);
         svg9.call(xAxis);
 
-        d3.select('#axesXlabel').text(xIndicator).attr("transform", "translate("+(this.margin.left+this.width/2)+","+(this.height+this.margin.top+40)+") scale(1,1)");
-        d3.select('#axesYlabel').text(yIndicator).attr("transform", "translate(20,"+(this.height/2) +") rotate(-90)");
+        d3.select('#axesXlabel').text(xIndicator).attr("transform", "translate("+(this.margin.left+this.width/2)+","+(this.height+this.margin.top+30)+") scale(1,1)");
+        d3.select('#axesYlabel').text(yIndicator).attr("transform", "translate(40,"+(this.height/2) +") rotate(-90)");
 
 
         let yScale = d3.scaleLinear()
-            .domain([0, maxvals[2]])
-            .range([this.height+this.margin.top, this.margin.top]);
+            .domain([minvals[2], maxvals[2]])
+            .range([this.height+this.margin.top-5, this.margin.top+10]);
 
 
         let yAxis = d3.axisLeft();
@@ -186,71 +188,7 @@ class GapPlot {
 
 
 
-
-
-
-
-
-        let svgcircle = d3.select('#chart-view').select('.plot-svg').select('.wrapper-group');
-        let svgcircle2 = svgcircle.selectAll('circle').data(this.data);
-        svgcircle2.exit().remove();
-
-        let svgcircle2Enter = svgcircle2
-            .enter().append('circle');
-
-        svgcircle2 = svgcircle2Enter.merge(svgcircle2);
-
-        var plotTooltip = d3.select("body")
-        .append("div")
-        .attr("class", "sunburst-tooltip")
-        .style("visibility", "hidden")
-
-        var colorScale = d3.scaleOrdinal(d3.quantize(d3.interpolateSinebow, 5))
-
-        svgcircle2
-            .attr("cx", d => +d[xIndicator] / maxvals[1] * this.width)
-            .attr("cy", d => +d[yIndicator] / maxvals[2] * this.height)
-            .attr('class',d=>('SC_circles'))
-            .style("fill", d => colorScale(d.StructType))
-            .attr('id',d=>'SC'+d['BuildingId'])
-            .attr("r", d => circleSizer(+d[circleSizeIndicator])).attr("transform", "translate("+this.margin.left+","+(this.height+this.margin.top)+") scale(1,-1)");
-        // let tooltip = d3.select('.tooltip');
-        svgcircle2.on("mouseover", function(d){
-            return plotTooltip.style("visibility", "visible")
-                .style("width", "fit-content")
-                .style("height", "fit-content")
-                .style("border-radius", "10px")
-                .style("display", "flex")
-                .style("flex-direction", "column")
-                .style("justify-content", "center")
-                .html(that.tooltipRender(d,circleSizeIndicator) + "<br/>")
-            })
-            .on("mousemove", function(d){return plotTooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-            .on("mouseout", function(){return plotTooltip.style("visibility", "hidden");});
-
-
-        // .on('mouseover', function(d, i) {
-        //     //show tooltip
-        //     tooltip.transition()
-        //         .duration(200)
-        //         .style("opacity", .9);
-        //     tooltip.html(that.tooltipRender(d,circleSizeIndicator) + "<br/>")
-        //         .style("left", (d3.event.pageX) + "px")
-        //         .style("top", (d3.event.pageY - 238) + "px");
-
-        // });
-
-        //hover function for country selection
-        // svgcircle2.on("mouseout", function(d) {
-        //     tooltip.transition()
-        //         .duration(500)
-        //         .style("opacity", 0);
-        // });
-        svgcircle2.on('click', (d) => {
-            that.updateHighlight(d)
-        });
-
-        const brushGroup = d3.select('.plot-svg').insert('g',":first-child").classed("brush", true);
+        const brushGroup = d3.select('.plot-svg').select(".brush");
 
         const brush = d3
             .brush()
@@ -318,6 +256,53 @@ class GapPlot {
                 }
             });
         brushGroup.call(brush);
+
+
+
+
+
+        let svgcircle = d3.select('#chart-view').select('.plot-svg').select('.wrapper-group');
+        let svgcircle2 = svgcircle.selectAll('circle').data(this.data);
+        svgcircle2.exit().remove();
+
+        let svgcircle2Enter = svgcircle2
+            .enter().append('circle');
+
+        svgcircle2 = svgcircle2Enter.merge(svgcircle2);
+
+        var plotTooltip = d3.select("body")
+        .append("div")
+        .attr("class", "sunburst-tooltip")
+        .style("visibility", "hidden")
+
+        var colorScale = d3.scaleOrdinal(d3.quantize(d3.interpolateSinebow, 6))
+
+        svgcircle2
+            .attr("cx", d => xScale(+d[xIndicator] ))
+            .attr("cy", d => yScale(+d[yIndicator] ) )
+            .attr('class',d=>('SC_circles'))
+            .style("fill", d => colorScale(d.StructType))
+            .attr('id',d=>'SC'+d['BuildingId'])
+            .attr("r", d => circleSizer(+d[circleSizeIndicator]))//.attr("transform", "translate("+this.margin.left+","+(this.height+this.margin.top)+") scale(1,-1)");
+        // let tooltip = d3.select('.tooltip');
+        svgcircle2.on("mouseover", function(d){
+            return plotTooltip.style("visibility", "visible")
+                .style("width", "fit-content")
+                .style("height", "fit-content")
+                .style("border-radius", "10px")
+                .style("display", "flex")
+                .style("flex-direction", "column")
+                .style("justify-content", "center")
+                .html(that.tooltipRender(d,circleSizeIndicator) + "<br/>")
+            })
+            .on("mousemove", function(d){return plotTooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+            .on("mouseout", function(){return plotTooltip.style("visibility", "hidden");});
+
+        svgcircle2.on('click', (d) => {
+            that.updateHighlight(d)
+        });
+
+
 
     }
 
@@ -437,7 +422,7 @@ class GapPlot {
         circleGroup.select('circle').attr('cy', '0');
         let numText = circleGroup.select('text').text(d => new Intl.NumberFormat().format(d));
 
-        var colorScale = d3.scaleOrdinal(d3.quantize(d3.interpolateSinebow, 5))
+        var colorScale = d3.scaleOrdinal(d3.quantize(d3.interpolateSinebow, 6))
 
         numText.attr('transform', (d) => 'translate(' + ((scale(d)) + 10) + ', 0)');
 
@@ -464,7 +449,7 @@ let typeData=[
         circleEnterType.append('text').classed('circle-size-text', true);
         circleGroupType = circleEnterType.merge(circleGroupType);
 
-        circleGroupType.attr('transform', (d, i) => 'translate(' +(i*24+30)+',' + 70 + ')');
+        circleGroupType.attr('transform', (d, i) => 'translate(' +(i*30+30)+',' + 70 + ')');
         circleGroupType.select('circle').attr('r', 5);
         circleGroupType.select('circle').attr('cx', '0');
         circleGroupType.select('circle').attr('cy', '0');
